aefd4b96e00ab7f85f83deb83c0dd73e5e9a483c
Paul Lang <paul.andrew.lang@gmail.com>
2012-08-05 23:13:13 -0700

    add stub routes with JSON

--- web.js
+++ web.js
@@ -2,8 +2,36 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/recommendations/1', function(req, res) {
+	/*
+	var r = getRecommendations().recommendations;
+	var id = req.params.id;
+	var idx = Integer.parseInt(id >= 0 && id < r.length ? id : 0);
+	res.send( r[id] );
+	*/
+	res.send(getRecommendations().recommendations[1])
+});
+
+app.get('/recommendations/', function(request, response) {
+	response.send( getRecommendations() );
+});
+
+function getRecommendations() {
+
+	return {
+		"recommendations": [
+				{"user" : "joel", "value" : "#London2012"},
+				{"user" : "paul", "value" : "#TeamUSA"},
+				{"user" : "@MarsCuriosity", "value" : "#MSL"}
+		]
+	};
+	
+}
+
+app.post('/recommended', function(request,response) {
+	// TODO: Validate input from client
+	lastRecommended = request.body;
+	lastRecommended = "#posted"
 });
 
 var port = process.env.PORT || 5000;
